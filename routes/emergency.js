3d81d562484b64f22cde4714220b7e937d6853a2
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-09-17 21:26:04 -0700

    Update calls to routes/emergency to dynamically create an alert message containing info specific to that user  and send a text message to his/her specified emergency contact number when the button is pushed.

--- routes/emergency.js
+++ routes/emergency.js
@@ -23,13 +23,37 @@ router.get('/:user_id', function (req, res, next) {
     .select()
     .where('id', req.params.user_id)
     .then((data) => {
-      console.log('data', data)
+      //console.log('data', data)
       userInfo = data;
+      userInfo.from = "+16504851985"; //your bandwidth number
+      userInfo.to = "+15109383112"; //my phone number
+      console.log('userInfo contains the following data ', userInfo);
       res.send(data)
     })
     .then(() => {
       console.log('user info is: ', userInfo);
+      sendMessage(userInfo);
     })
-})
+});
+
+  var sendMessage = function(params){
+    client.Message.send({
+      //returns a promise
+      from : params.from, //your bandwidth number
+      to   : params.to, //number to send to
+      text : `ATTENTION: ${params[0].emergency_contact_name}!!! This is an automated alert message from the k9-1-1 App.  ${params[0].name} requires emergency medical services for the following medical condition/s ${params[0].condition} at GPS coordinates 135.7 and 96.4.`,
+      media: "https://s3.amazonaws.com/bwdemos/logo.png"
+    })
+    //calls back the message id number and catches any errors
+    .then(function(message){
+      console.log(message);
+      return client.Message.get(message.id)
+      //access ID from json can also get to and from
+    })
+    // catches any errors
+    .catch(function(err){
+      console.log(err)
+    })
+  }
 
 module.exports = router
